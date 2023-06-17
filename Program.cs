47c74227e167835ce22bae1af63ba72d126dafde
IvanBzhaniya <ivan.bzhaniya@gmail.com>
2023-06-17 19:27:08 +0300

    ex3

--- Program.cs
+++ Program.cs
@@ -32,7 +32,7 @@ else
 Задача 21
 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-*/
+
 
 double LongLineABC (int xA, int xB, int yA, int yB, int zA, int zB)
 {
@@ -56,3 +56,40 @@ int zCoordB = Convert.ToInt32(Console.ReadLine());
 
 double dist = LongLineABC(xCoordA, xCoordB, yCoordA, yCoordB, zCoordA, zCoordB);
 Console.WriteLine($"distance between A({xCoordA} + {yCoordA} + {zCoordA}) and B({xCoordB} + {yCoordB} + {zCoordB}) is {dist}");
+
+
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+*/
+
+void SqurePrinter(int numberN)
+{
+        int current = 1;
+        while (current <= numberN)
+        {
+                double res = Math.Pow(current,3);
+                Console.Write($"{res:f0}, ");
+                current++;
+        }
+        Console.WriteLine("\b\b.");
+}
+void SqurePrinterNeg(int numberN)
+{
+        int current = 1;
+        while (current >= numberN)
+        {
+                double res = Math.Pow(current,3);
+                Console.Write($"{res:f0}, ");
+                current--;
+        }
+        Console.WriteLine("\b\b.");
+}
+
+Console.Write("Input number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+
+if (user_number > 0)
+        SqurePrinter(user_number);
+else
+        SqurePrinterNeg(user_number);
